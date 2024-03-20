9bc20d705eac60c8e8537091b9314c93be4b6d41
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-20 13:06:18 -0600

    Cambio en modelos

---
+++ api/src/models/Movie.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+    sequelize.define('Movie', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        poster: {
+            //Provisional
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        director: {
+            //Provisional
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        genre: {
+            //Provisional
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        duration: {
+            //Provisional
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        country: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+    })
+}
