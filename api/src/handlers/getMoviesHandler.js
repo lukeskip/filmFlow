c738ba03253e1f6701185e42dc411db34251df87
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-19 22:17:02 -0600

    Probando y creando las Rutas

---
+++ api/src/handlers/getMoviesHandler.js
@@ -0,0 +1,10 @@
+const getMovies = require('../controllers/getMovies')
+
+module.exports = async function getMoviesHandler(req, res){
+    try {
+        const data = await getMovies()
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,13 @@
 const {Router} = require('express')
 const {movies} = require('./api')
+const postContentHandler = require('../handlers/postContentHandler')
+const getMoviesHandler = require('../handlers/getMoviesHandler')
 const router = Router()
 
-router.get('/movies', async (req, res)=>{
+router.get('/fake', async (req, res)=>{
     res.status(200).json(movies)
 })
+router.get('/movies', getMoviesHandler)
+router.post('/movies', postContentHandler)
 
 module.exports = router
