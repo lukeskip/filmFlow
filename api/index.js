003e8fe9b1f419f10297d72cceb334ce51dc1e5d
Baidewei <gerant9seminario@gmail.com>
2024-03-21 11:52:27 -0400

    Test .env

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const saveGenres = require('./src/services/saveGenresToDB.js');
 const saveMovies = require("./src/services/saveMoviesToDB.js");
-
+const { TEST } = process.env;
 
 conn.sync({ force: true }).then(() => {
   server.listen(3001, async () => {
     try {
       await saveGenres();
-      await saveMovies();
+      if (TEST === "TRUE")
+        await saveMovies();
     } catch (error) {
       console.log(error);
     }
