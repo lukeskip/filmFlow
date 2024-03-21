013dc12472ac6189e63b7d939f54acd873d6e815
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-20 18:49:31 -0600

    Creando ruta MoviesiD y aplicando el ordenamiento a los filtros

--- api/src/routes/moviesRouter.js
+++ api/src/routes/moviesRouter.js
@@ -1,12 +1,15 @@
 const { Router } = require("express");
 const postMoviesHandler = require('../handlers/postMoviesHandler')
 const getMoviesHandler = require('../handlers/getMoviesHandler')
+const getMoviesIdHandler = require('../handlers/getMoviesIdHandler')
 const putMoviesHandler = require('../handlers/putMoviesHandler')
 
 const moviesRouter = Router();
 
 moviesRouter.get('/', getMoviesHandler);
+moviesRouter.get('/:id', getMoviesIdHandler);
 moviesRouter.post('/', postMoviesHandler);
 moviesRouter.put('/:id', putMoviesHandler);
+
 
 module.exports = moviesRouter;
