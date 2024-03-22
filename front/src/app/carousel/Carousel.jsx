5cd09087071c011313af74f34f175e01630659d7
julianfv1150 <julian.f.vega@gmail.com>
2024-03-22 10:42:30 -0300

    UPD: creating filterViews

--- front/src/app/carousel/Carousel.jsx
+++ front/src/app/carousel/Carousel.jsx
@@ -8,7 +8,6 @@ const Carousel = ({ movie }) => {
     useEffect(() => {
         setArrMovie(movie)
     }, [movie]);
-    console.log(movie);
     const changeMovie = (direct) => {
         if(direct === 'prev'){
             if(movieIndex > 0) return setMovieIndex(movieIndex-1)
@@ -23,7 +22,6 @@ const Carousel = ({ movie }) => {
             }
         }
     }
-
     return (
         <div className="container">
             <div className={style.mainContainer}>                
