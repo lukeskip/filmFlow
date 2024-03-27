a48097970ec30dd77db4eaf4cb57b4ebb97b04de
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-27 11:44:52 -0600

    fix en configuracion

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // Define la función generateStaticParams()
-  generateStaticParams: async () => {
-    // Puedes realizar operaciones asíncronas aquí si es necesario
-    return {
-      // Devuelve los parámetros estáticos que deseas precalcular
-      staticParams: {
-        key: 'value',
-        // Otros parámetros estáticos...
-      }
-    };
-  }
+    distDir: 'out',
 };
 export default nextConfig;
