23bd2ab385d51adc4b1a2cf0b5f0c0e0b565a873
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-26 18:55:38 -0600

    estilos componentes Movie y Movies se agrega componente Button

--- front/src/components/movie/Movie.jsx
+++ front/src/components/movie/Movie.jsx
@@ -2,16 +2,21 @@ import style from "./Movie.module.scss"
 import Buy from '../btnBuy/buy'
 
 const Movie = ({ elem, dim }) => {
+    const title = (title)=>{
+        return title.length >=15 ? title.slice(0,15)+"...":title;
+    }
     return(
         <div key={elem.id} className={style.card} >
-            <img 
-                src={elem.poster}
-                width={dim ? dim[0] : '200px'}
-                height={dim ? dim[1] : '300px'}
-            />
+           <div>
+                <img 
+                    src={elem.poster}
+                    width={dim ? dim[0] : '200px'}
+                    height={dim ? dim[1] : '300px'}
+                />
+           </div>
 
-            <p className={style.order}>{elem.name}</p>
-            <Buy movie = {elem}></Buy>
+            <div className={style.order}>{title(elem.name) }</div>
+            <div><Buy movie = {elem}></Buy></div>
 
         </div>
     )
